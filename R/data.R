54028e734d1b0d12a85aa224a6eb5c9ec5dd3e8f
BiliBraker <soirkorob@gmail.com>
2020-10-18 16:21:30 +0200

    update descriptions

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Keywords for preprocessing
 #'
 #' @format A vector containing keywords
-#' @source "C:\Users\soirk\Krisztian\Research\missing_data_paper\R_stuff\jprep\data\"
+#' @source manually made
 
 "keywords"
