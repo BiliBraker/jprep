7829dbdd4454d86c6fd275f200bc67b51e11e9ce
Krisztián Boros <51168364+BiliBraker@users.noreply.github.com>
2020-11-23 13:42:08 +0100

    update dataset sources

--- data-raw/keywords.R
+++ data-raw/keywords.R
@@ -1,3 +1,3 @@
 ## code to prepare `keywords` dataset goes here
-keywords <- load("./keywords.RData", envir = globalenv())
+load("./keywords.RData", envir = globalenv())
 usethis::use_data(keywords, overwrite = TRUE)
