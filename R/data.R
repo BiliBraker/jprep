967af61d67c66aa8ccfc3ecd48dcc8ce87d75cf6
Krisztián Boros <51168364+BiliBraker@users.noreply.github.com>
2020-11-23 13:41:49 +0100

    update dataset descriptions

--- R/data.R
+++ R/data.R
@@ -1,6 +1,27 @@
 #' Keywords for preprocessing
 #'
 #' @format A vector containing keywords
-#' @source manually made
-
+#' @source Manually made
 "keywords"
+
+#' Costum stopwords for preprocessing; excluding 'were', 'at', and 'not'
+#'
+#' @format A vector containing stopwords
+#' @source Manually made
+"stopwords_new"
+
+#' Costum Vocabulary
+#'
+#' A vocabulary made from "GradyAugmented" and "keywords"
+#'
+#' @format A vector
+#' @source GradyAugmented vocabulary from "qdapdictionaries" package + "keywords"
+"vocab"
+
+#' Discipline categories
+#'
+#' Reduced discipline categories
+#'
+#' @format A vector
+#' @source Manually made from jstor::jst_get_journal_overview
+"disc_reduced"
